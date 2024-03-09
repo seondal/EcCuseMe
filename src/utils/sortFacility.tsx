1ae53d3df11156fd647d55b5727a188d863f3fb4
seondal <sseondal@gmail.com>
2024-03-10 00:21:55 +0900

    🛠 fix :  .toSorted -> .sort

--- src/utils/sortFacility.tsx
+++ src/utils/sortFacility.tsx
@@ -2,8 +2,8 @@ import { Facilities } from "@/data/facilities";
 
 export const facilityNearGate3 = Facilities.filter(
   (item) => item.gate === 3
-).toSorted((a, b) => b.room - a.room);
+).sort((a, b) => b.room - a.room);
 
 export const facilityNearGate4 = Facilities.filter(
   (item) => item.gate === 4
-).toSorted((a, b) => a.room - b.room);
+).sort((a, b) => a.room - b.room);
