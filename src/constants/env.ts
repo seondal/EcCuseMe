6a9e01506a6d55a2994b028f4b9a6d0b54051a39
seondal <sseondal@gmail.com>
2024-03-11 19:21:47 +0900

    🌐 seo :  네이버 서치어드바이저

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,5 +1,6 @@
 const ENV = {
   gaTag: process.env.NEXT_PUBLIC_GA_TAG || "",
   adsense_account: process.env.NEXT_PUBLIC_ADSENSE_ACCOUNT || "",
+  naver_search_adviser: "c2392fafc8b7ed24033efa4c840fcf2e9dfb45e1",
 };
 export default ENV;
