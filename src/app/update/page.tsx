872ea9a4a811937c56d2fef10af120ca3a8cf0c1
seondal <sseondal@gmail.com>
2025-03-03 16:29:48 +0900

    피드백 링크 변경

--- src/app/update/page.tsx
+++ src/app/update/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
           </div>
         ))}
       </div>
-      <a href={URL.developer}>
+      <a href={URL.feedback}>
         <button>나도 의견 남기고 업데이트 참여하기</button>
       </a>
     </div>
--- src/components/GimmeFeedback.tsx
+++ src/components/GimmeFeedback.tsx
@@ -7,7 +7,7 @@ export default function GimmeFeedback() {
       {/* <div>문의, 기능제안, 오류신고 편하게 해주세요!</div>
       <div>보내주신 의견들을 바로 반영할게요</div> */}
       <div className="flex gap-4 justify-center mt-2 flex-wrap">
-        <a href={URL.developer}>
+        <a href={URL.feedback}>
           <button>이 사이트 만든 사람</button>
         </a>
         <Link href={"/update"}>
