723ec8b5d53f4c32e013ab8a677db1ac83b0abd7
seondal <sseondal@gmail.com>
2025-06-16 21:22:07 +0900

    📦 add :  노션 폼

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,6 +1,6 @@
 const URL = {
   developer: "https://litt.ly/seondal",
-  feedback: "https://forms.gle/t7ynpwgQeVYjPY3Y6",
+  feedback: "https://seondal.notion.site/213d88b98263808c935bc470bc86e6f4",
   cybercampus:
     "https://whkakrkr.tistory.com/entry/%EC%82%AC%EC%9D%B4%EB%B2%84%EC%BA%A0%ED%8D%BC%EC%8A%A4-%EA%B0%95%EC%9D%98-%EB%8B%A4%EC%9A%B4-%EB%B0%9B%EC%95%84%EC%84%9C-%EC%A0%80%EC%9E%A5%ED%95%98%EA%B8%B0",
 };
