1b0fc5101f8c3eb3e9747ce833f5f27e07ced188
seondal <sseondal@gmail.com>
2025-07-11 22:05:26 +0900

    ✂️ edit :  피자 링크 변경

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const LINK =
-  "https://map.naver.com/p/search/%EC%B5%B8%EC%9D%B4%ED%94%BC%EC%9E%90/place/20859695?placePath=%3Fentry%3Dpll%26from%3Dnx%26fromNxList%3Dtrue&placeSearchOption=entry%3Dpll%26fromNxList%3Dtrue%26x%3D126.954686%26y%3D37.564827&searchType=place" as const;
+const LINK = "https://naver.me/xDs6vnPb" as const;
 
 export default function Ad() {
   return (
